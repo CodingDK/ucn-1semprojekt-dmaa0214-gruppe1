8fdab750f981248b21f88a53044088de56a41bcd
lau.kondrup <lau.kondrup@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-05-23 11:39:37 +0000

    UI lidt af hvert

--- src/uiLayer/PersonUI.java
+++ src/uiLayer/PersonUI.java
@@ -1,5 +1,59 @@
 package uiLayer;
 
 public class PersonUI extends SuperUI{
+	
+	public PersonUI(){
+		
+	}
+	
+	
+	public void createPrivate() {
+	
+	}
+
+
+	public void createBusiness() {
+		
+	}
+
+	public void createSeller() {
+		// TODO Auto-generated method stub
+	}
+
+	public void createSupplier() {
+		// TODO Auto-generated method stub
+	}
+
+	public void createAdministrator() {
+		// TODO Auto-generated method stub
+	}
+
+	public void updatePrivate() {
+		// TODO Auto-generated method stub
+	}
+
+	public void updateBusiness() {
+		// TODO Auto-generated method stub
+	}
+
+	private void updateSeller() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void updateSupplier() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void updateAdministrator() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void removePerson() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
