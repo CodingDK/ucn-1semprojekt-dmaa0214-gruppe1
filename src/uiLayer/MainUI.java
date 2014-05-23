941de2c0c1e1214c7a86a310e252790f08dc7eb7
lau.kondrup <lau.kondrup@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-05-23 11:16:29 +0000

    MainUI lavet.

--- src/uiLayer/MainUI.java
+++ src/uiLayer/MainUI.java
@@ -1,5 +1,49 @@
 package uiLayer;
 
-public class MainUI {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class MainUI {
+	
+	private MainUI(){
+		menu();
+	}
+	
+	private void menu(){
+		boolean exit = false;
+		while(!exit){
+			int choice = writeMenu();
+			if(choice == 1){
+				new PersonUI();
+			} else if(choice == 2){
+				new SaleUI();
+			} else if(choice == 3){
+				new ItemUI();
+			} else if(choice == 4){
+				System.out.println("System is shutting down.");
+				exit = true;
+			}
+		}
+	}
+	
+	private int writeMenu(){
+		int choice = 0;
+		try{
+			System.out.println("1. Kunde UI");
+			System.out.println("2. Salg UI");
+			System.out.println("3. Vare UI");
+			System.out.println("4. Exit");
+			Scanner k = new Scanner(System.in);
+			choice = k.nextInt();
+		} catch(InputMismatchException e){
+			System.out.println(e);
+		}
+		return choice;
+		
+	}
+	
+	public static void main(String[] args){
+		new MainUI();
+	}
+	
 }
