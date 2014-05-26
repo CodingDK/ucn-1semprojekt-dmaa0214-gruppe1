11545113409b009866e0c712587dc59898cacbc0
nonames.dk@gmail.com <nonames.dk@gmail.com@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-05-26 10:02:08 +0000

    Storage og StorageCont ver. 0.1

--- trunk/src/modelLayer/Storage.java
+++ trunk/src/modelLayer/Storage.java
@@ -1,5 +1,47 @@
 package modelLayer;
 
-public class Storage {
+/**
+ * Storage holds information of a storage.
+ * 
+ * @author Group 1
+ * @version 0.1
+ */
+public class Storage{
+	private static int idIterator;
+	private int id;
+	private String name;
 
+	/**
+	 * Constructor of the class Storage
+	 * @param name The name of the storage.
+	 */
+    public Storage(String name) {
+    	idIterator++;
+    	this.name = name;
+    	this.id = idIterator;
+    }
+
+    /**
+    * getId - Returns the Id
+	* @return id
+    */
+	public int getId(){
+		return id;
+	}
+
+    /**
+    * getName - Returns the Name
+	* @return name
+    */
+	public String getName(){
+		return name;
+	}
+
+    /**
+    * setName - sets the Name
+	* @param String name
+    */
+	public void setName(String name){
+		this.name = name;
+	}
 }
