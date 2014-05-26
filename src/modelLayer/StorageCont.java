7e32c6748833a07dc64c06b8ab5593541e39b781
CodingDK <nonames.dk@gmail.com>
2014-05-26 10:02:08 +0000

    Storage og StorageCont ver. 0.1

--- src/modelLayer/StorageCont.java
+++ src/modelLayer/StorageCont.java
@@ -1,5 +1,39 @@
 package modelLayer;
+import java.util.ArrayList;
 
-public class StorageCont {
+/**
+ * StorageCont contains a list of all storage in the StorageCont.
+ * 
+ * @author Group 1
+ * @version 0.1
+ */
+public class StorageCont{
+	private static StorageCont instance;
+	private ArrayList<Storage> storage;
+	
+	/**
+	 * Constructor for objects of class StorageCont
+	 */
+	private StorageCont(){
+		storage = new ArrayList<Storage>();
+	}
+	
+	/**
+     * getInstance - Get only one instance of the StorageCont.
+     * @return StorageCont - The instance of the StorageCont.
+     */
+	public static StorageCont getInstance(){
+		if(instance == null){
+			instance = new StorageCont();
+		}
+		return instance;
+	}
 
+	/**
+     * addStorage - Add a Storage object to the StorageCont.
+     * @param s The Storage object to add.
+     */
+	public void addStorage(Storage s){
+		storage.add(s);
+	}
 }
