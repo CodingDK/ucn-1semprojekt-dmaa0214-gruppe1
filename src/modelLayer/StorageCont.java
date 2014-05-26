52277ac7b65c7c2dd42b27d63e5d0fda12ed446d
CodingDK <nonames.dk@gmail.com>
2014-05-26 12:16:52 +0000

    ItemCtr og dertilhørende

--- src/modelLayer/StorageCont.java
+++ src/modelLayer/StorageCont.java
@@ -1,6 +1,8 @@
 package modelLayer;
 import java.util.ArrayList;
 
+import personLayer.Employee;
+
 /**
  * StorageCont contains a list of all storage in the StorageCont.
  * 
@@ -9,13 +11,13 @@ import java.util.ArrayList;
  */
 public class StorageCont{
 	private static StorageCont instance;
-	private ArrayList<Storage> storage;
+	private ArrayList<Storage> storages;
 	
 	/**
 	 * Constructor for objects of class StorageCont
 	 */
 	private StorageCont(){
-		storage = new ArrayList<Storage>();
+		storages = new ArrayList<Storage>();
 	}
 	
 	/**
@@ -34,6 +36,34 @@ public class StorageCont{
      * @param s The Storage object to add.
      */
 	public void addStorage(Storage s){
-		storage.add(s);
+		storages.add(s);
+	}
+	
+	 /**
+     * findStorage - Get a Storage object from the name of the Storage.
+     * @param name - The name of the Storage.
+     * @return Storage object of the Storage or null if not found.
+     */
+	public Storage findStorage(String name){
+		Storage retStorage = null;
+    	boolean found = false;
+    	int i = 0;
+    	while(i < storages.size() && !found) {
+    		if(storages.get(i).getName().equalsIgnoreCase(name)) {
+    			retStorage = storages.get(i);
+    			found = true;
+    		}
+    		i++;
+    	}
+    	
+    	return retStorage;
+	}
+	
+	/**
+	 * removeStorage - Remove a Storage object from the container.
+	 * @param s The Storage object to remove.
+	 */
+	public void removeStorage(Storage s){
+		storages.remove(s);
 	}
 }
