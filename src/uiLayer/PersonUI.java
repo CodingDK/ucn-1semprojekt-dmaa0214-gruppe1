b71dfc8798850886da7a4868d52809e16d7f89d7
lasse.k2 <lasse.k2@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-05-26 08:29:14 +0000

    PersonUi og SuperUi

--- src/uiLayer/PersonUI.java
+++ src/uiLayer/PersonUI.java
@@ -1,21 +1,89 @@
 package uiLayer;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import ctrLayer.CustomerCtr;
+
 public class PersonUI extends SuperUI{
 	
 	public PersonUI(){
+	
+		boolean exit = false;
+		while(!exit){
+			
+			int choice = writeMenu();
+			if(choice == 1){
+				createPrivate();
+			}
+			if(choice ==2){
+				createBusiness();
+			}
+		}
 		
 	}
 	
+	private int writeMenu(){
+		int choice = 0;
+		try{
+			System.out.println("1. Opret privat kunde");
+			System.out.println("2. Opret erhvervs kunde");
+			Scanner k = new Scanner(System.in);
+			choice = k.nextInt();
+		} catch(InputMismatchException e){
+			System.out.println("Forkert input!");
+		}
+		return choice;
+	}
 	
+	
+	/**
+	 * createPrivate - Create privat customer
+	 */
 	public void createPrivate() {
-	
+		try{
+			Scanner k = new Scanner(System.in);
+			System.out.println("## Opret privat kunde ##");
+			
+			System.out.print("Navn: ");
+			String name = k.nextLine();			
+			
+			System.out.print("Vej navn: ");
+			String street  = k.nextLine();
+			
+			System.out.print("Postnummer: ");
+			String postCode = k.nextLine();
+			
+			System.out.print("By: ");
+			String city = k.nextLine();
+			
+			System.out.print("Telefon nr: ");
+			String phoneNr = k.nextLine();
+			
+			System.out.print("E-mail: ");
+			String email = k.nextLine();
+			
+			System.out.print("CPR nr: ");
+			String cprNr = k.nextLine();
+			
+			System.out.print("Billede ID: ");
+			String pictureId = k.nextLine();
+			
+			CustomerCtr customerCtr = new CustomerCtr();
+			customerCtr.createPrivateCustomer(name, phoneNr, street, email, city, postCode, cprNr, pictureId);
+		}
+		catch(InputMismatchException e){
+			System.out.println("Forket input!");
+		}
+		
+		
 	}
 
 
 	public void createBusiness() {
 		
 	}
-
+	
 	public void createSeller() {
 		// TODO Auto-generated method stub
 	}
