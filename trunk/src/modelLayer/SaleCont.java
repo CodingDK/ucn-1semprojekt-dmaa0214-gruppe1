dfd3a603213dd592888cce690ea82ce13eedbd31
lau.kondrup <lau.kondrup@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-05-27 08:12:54 +0000

    Sale Container.

--- trunk/src/modelLayer/SaleCont.java
+++ trunk/src/modelLayer/SaleCont.java
@@ -1,5 +1,33 @@
 package modelLayer;
+import java.util.ArrayList;
 
 public class SaleCont {
-
+	private static SaleCont instance;
+	private ArrayList<Sale> sales; 
+	
+	/**
+	 * Singleton container of the container class SaleCont.
+	 */
+	private SaleCont(){
+		sales = new ArrayList<Sale>();
+	}
+	
+	/**
+	 * Get instance method.
+	 * @return SaleCont - The instance of the Sale Container.
+	 */
+	public static SaleCont getInstance(){
+		if(instance == null){
+			instance = new SaleCont();
+		}
+		return instance;
+	}
+	
+	/**
+	 * Adds a sale object to the list of sales.
+	 * @param s Sale object.
+	 */
+	public void addSale(Sale s){
+		sales.add(s);
+	}
 }
