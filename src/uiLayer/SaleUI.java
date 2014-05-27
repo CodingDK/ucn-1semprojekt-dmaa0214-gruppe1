681f477a7e1341d32e6a36f6a6326200cb48f6ce
lau.kondrup <lau.kondrup@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-05-27 09:39:30 +0000

    Lidt af Salg UI

--- src/uiLayer/SaleUI.java
+++ src/uiLayer/SaleUI.java
@@ -1,5 +1,49 @@
 package uiLayer;
 
-public class SaleUI extends SuperUI{
+import java.util.Scanner;
 
+public class SaleUI extends SuperUI{
+		
+	public SaleUI(){
+		sCtr.createSale();
+		boolean exit = false;
+		while(!exit){
+			int choice = writeSaleMenu();
+			if(choice == 1){
+				addPartSale();
+			} else if(choice == 2){
+				createCustomer();
+			} else if (choice == 3){
+				searchCustomer();
+			} else if(choice == 4){
+				finishSale();
+			} else if(choice == 5){
+				exit = true
+				new MainUI();
+			}
+	}
+	
+	private int writeSaleMenu(){
+		int choice = 0;
+		try{
+			System.out.println("## Opret salg ##");
+			System.out.println("1. Tilføj vare");
+			System.out.println("2. Opret kunde");
+			System.out.println("3. Søg kunde");
+			System.out.println("4. Udfør salg");
+			System.out.println("5. Gå tilbage");
+			Scanner k = new Scanner(System.in);
+			choice = k.nextInt();		
+		} catch(InputMismatchException e){
+			System.out.println("Forkert input!")
+		}
+		return choice;
+	}
+	
+	/*
+	private void createSale(){
+	
+	}
+	*/
+	
 }
