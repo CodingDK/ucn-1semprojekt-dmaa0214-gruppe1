46269a6bfec91830aa337325616fb14713531823
lau.kondrup <lau.kondrup@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-05-27 08:06:12 +0000

    PartSale.

--- trunk/src/modelLayer/PartSale.java
+++ trunk/src/modelLayer/PartSale.java
@@ -1,5 +1,22 @@
 package modelLayer;
 
 public class PartSale {
+	
+	private static idIterator = 1000;
+	int id;
+	int amount;
+	Item item;
+	
+	/**
+	 * Constructor of the class PartSale
+	 * @param i The Item associated with the PartSale.
+	 * @param amount Amount of item.
+	 */
+	public PartSale(Item i, int amount){
+		id = idIterator;
+		idIterator++;
+		item = i;
+		this.amount = amount;
+	}
 
 }
