aab32776228ba8949142365dab1cadf02ccb95f1
nonames.dk@gmail.com <nonames.dk@gmail.com@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-05-27 11:46:39 +0000

    Test oprettet i mainUI

--- trunk/src/uiLayer/MainUI.java
+++ trunk/src/uiLayer/MainUI.java
@@ -3,6 +3,10 @@ package uiLayer;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import modelLayer.*;
+import personLayer.*;
+import ctrLayer.*;
+
 public class MainUI extends SuperUI{
 	
 	private MainUI(){
@@ -11,6 +15,7 @@ public class MainUI extends SuperUI{
 	
 	private void menu(){
 		boolean exit = false;
+		boolean testCreated = false;
 		while(!exit){
 			int choice = writeMenu();
 			if(choice == 1){
@@ -20,6 +25,14 @@ public class MainUI extends SuperUI{
 			} else if(choice == 3){
 				new ItemUI();
 			} else if(choice == 4){
+				if(testCreated){
+					System.out.println("Test allerede oprettet");
+					pause();
+				} else {
+					makeTest();
+					testCreated = true;
+				}
+			} else if(choice == 5){
 				System.out.println("Program afsluttet.");
 				exit = true;
 			}
@@ -33,13 +46,41 @@ public class MainUI extends SuperUI{
 			System.out.println("1. Kunde UI");
 			System.out.println("2. Salg UI");
 			System.out.println("3. Vare UI");
-			System.out.println("4. Afslut program");
+			System.out.println("4. Lav Test");
+			System.out.println("5. Afslut program");
 			choice = k.nextInt();
 			k.nextLine();
 		} catch(InputMismatchException e){
 			System.out.println("Forkert input!");
 		}
 		return choice;
+	}
+	
+	private void makeTest(){
+		ItemCtr iCtr = new ItemCtr();
+		CategoryCtr cCtr = new CategoryCtr();
+		iCtr.createStorage("Lager1");
+		iCtr.createStorage("Lager2");
+		Storage s1 = iCtr.findStorage("Lager1");
+		Storage s2 = iCtr.findStorage("Lager2");
+		cCtr.createCategory("Søm");
+		cCtr.createCategory("Hammer");
+		Category c1 = cCtr.findCategory("Søm");
+		Category c2 = cCtr.findCategory("Hammer");
+		
+		iCtr.createItem("Søm Flad", 200, 0, 1., 1., 1., 1, "1234", s1, 10, 1, c1);
+		iCtr.createItem("Søm t. Sømpistol", 200, 0, 1., 1., 1., 1, "1234", s1, 10, 1, c1);
+		
+		iCtr.createItem("Flad Hammer", 200, 0, 1., 1., 1., 1, "1234", s2, 10, 1, c2);
+		iCtr.createItem("Rund Hammer", 200, 0, 1., 1., 1., 1, "1234", s2, 10, 1, c2);
+		
+		EmployeeCtr eCtr = new EmployeeCtr();
+		eCtr.createEmployee("1", "Ole", "70809010", "Egonsvej 19", "ole@ucn.dk", "Aalborg", "9000", "201050-1043", true);
+		eCtr.createEmployee("2", "Jens", "40509010", "Hobrovej 29", "jens@ucn.dk", "Vestbjerg", "9380", "100170-2143", false);
+		
+		CustomerCtr cusCont = new CustomerCtr();
+		cusCont.createPrivateCustomer("Bjarne", "12345678", "Lærkevej 2", "bjarne@ft.dk", "Aalborg", "9000", "121248-3010", "43432535");
+		cusCont.createBusinessCustomer("Kis", "72691867", "Sofiendalsvej 60", "kbha@ucn.dk", "Aalborg", "9000", "UCN A/S", "33556063");
 	}
 	
 	public static void main(String[] args){
