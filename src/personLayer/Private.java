1024afc63e52cd53900e043bc7606548f2f71371
nonames.dk@gmail.com <nonames.dk@gmail.com@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-06-02 06:38:30 +0000

    personLayer doc

--- src/personLayer/Private.java
+++ src/personLayer/Private.java
@@ -1,8 +1,26 @@
 package personLayer;
 
+/**
+ * Private holds information for private customers.
+ * 
+ * @author Group 1
+ * @version 0.1
+ * 
+ */
 public class Private extends Customer{
 	private String cprNr, pictureID;
 	
+	/**
+	 * Constructor for Private objects.
+     * @param name The name of the privateCustomer.
+     * @param phoneNr The phoneNr of the privateCustomer.
+     * @param street The street of the privateCustomer.
+     * @param email The email of the privateCustomer.
+     * @param city The city of the privateCustomer.
+     * @param postCode The postCode of the privateCustomer.
+	 * @param cprNr The cprNr of the privateCustomer.
+	 * @param pictureID The pictureID of the privateCustomer.
+	 */
 	public Private(String name, String phoneNr, String street, String email, String city, String postCode, String cprNr, String pictureID){
 		super(name, phoneNr, street, email, city, postCode);
 		this.cprNr = cprNr;
@@ -23,6 +41,9 @@ public class Private extends Customer{
 		this.pictureID = pictureID;
 	}
 	
+	/**
+	 * @return String with superclass toString and name.
+	 */
 	public String toString(){
 		return super.toString() + " Navn: " + name;
 	}
