30166a785ea7bce5f4e4de2534aaa7547ebf3456
lasse.k2 <lasse.k2@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-06-10 12:28:53 +0000

    CustomerGUI

--- src/exceptionLayer/SaleNotCreatedException.java
+++ src/exceptionLayer/SaleNotCreatedException.java
@@ -18,10 +18,4 @@ public class SaleNotCreatedException extends Exception {
 	public SaleNotCreatedException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
 	}
-	
-	public SaleNotCreatedException(String arg0, Throwable arg1, boolean arg2,
-			boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-	}
-	
 }
