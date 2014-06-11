680923f80bd25662713b578abe84827689f42878
lasse.k2 <lasse.k2@27ca16b6-6f16-e4c0-0d48-d5c2e8cd46f8>
2014-06-11 11:09:57 +0000

    Rette i StorageGUI og tilføjet StorageTableModel klassen

---
+++ src/uiLayer/StorageTableModel.java
@@ -0,0 +1,68 @@
+package uiLayer;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import modelLayer.Storage;
+
+public class StorageTableModel extends AbstractTableModel{
+	private static final long serialVersionUID = 1L;
+	private ArrayList<Storage> storages;
+	
+	public StorageTableModel(ArrayList<Storage> stor){
+		this.storages = stor;
+	}
+
+	public void refresh(ArrayList<Storage> s) {
+		this.storages = s;
+	}
+
+	public int getColumnCount() {
+		return 6;
+	}
+
+	public int getRowCount() {
+		return storages.size();
+	}
+
+	public Object getValueAt(int rowIndex, int collIndex) {
+		Storage s  = storages.get(rowIndex);
+		Object value = null;
+		if(collIndex == 0){
+			value = s.getId();
+		} else if(collIndex == 1){
+			value = s.getName();
+		} else if(collIndex == 2){
+			value = "";
+		} else if(collIndex == 3){
+			value = "";
+		} else if(collIndex == 4){
+			value = "";
+		} else if(collIndex == 5){
+			value = "";
+		} 
+		return value;
+	}	
+	
+	public String getColumnName(int collIndex){
+		
+		String value = "??";
+		
+		if(collIndex == 0){
+			value = "ID";
+		} else if(collIndex == 1){
+			value = "Navn";
+		} else if(collIndex == 2){
+			value = "Gade";
+		} else if(collIndex == 3){
+			value = "By";
+		} else if(collIndex == 4){
+			value = "Postnummer";
+		} else if(collIndex == 5){
+			value = "Tlf nr";
+		}
+		
+		return value;
+	}
+}
